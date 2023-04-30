6d03621ca3f671ce4ab08bd92ac4df54367ff6b0
mhaikal167 <skadskuds.f@gmail.com>
2023-04-30 15:26:00 +0700

    update all stuff to day 14

---
+++ connection/connection.go
@@ -0,0 +1,26 @@
+package connection
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v4"
+)
+
+var Conn *pgx.Conn
+
+func DatabaseConnect(){
+
+	databaseUrl := "postgresql://postgres:167669123@localhost:5432/db_project"
+
+	var err error
+	Conn,err = pgx.Connect(context.Background(),databaseUrl)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr,"unable to connect to database : %v\n",err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Success connect to database 👌👌")
+}
--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"personal-web-week-3/connection"
 	"strconv"
 	"text/template"
 	"time"
 
+	"github.com/jackc/pgtype"
 	"github.com/labstack/echo/v4"
 )
 
@@ -16,39 +19,19 @@ type Project struct {
 	Start    string
 	End      string
 	Desc     string
-	Node     bool
-	React    bool
-	Next     bool
-	Typesc   bool
+	Techno   []string
 	Duration int
+	PostDate string
 }
 
-var dataProject = []Project{
-	{
-		Title:    "Sapi terbang jualan kuah sayur",
-		Start:    "2023-04-15",
-		End:      "2023-06-21",
-		Desc:     "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia temporibus mollitia impedit repudiandae? Non, assumenda! Dignissimos sed, accusamus aspernatur itaque at ut perferendis consequatur dolorem ad temporibus corrupti molestiae id, iusto saepe sint iure. Praesentium assumenda, eveniet placeat explicabo perferendis, consequuntur nesciunt optio corrupti quaerat error porro officiis. Cupiditate, vitae!",
-		Node:     true,
-		React:    true,
-		Next:     false,
-		Typesc:   true,
-		Duration: 234,
-	},
-	{
-		Title:    "Kuda terbang jualan kuah sayur",
-		Start:    "2023-02-15",
-		End:      "2023-08-21",
-		Desc:     "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia temporibus mollitia impedit repudiandae? Non, assumenda! Dignissimos sed, accusamus aspernatur itaque at ut perferendis consequatur dolorem ad temporibus corrupti molestiae id, iusto saepe sint iure. Praesentium assumenda, eveniet placeat explicabo perferendis, consequuntur nesciunt optio corrupti quaerat error porro officiis. Cupiditate, vitae!",
-		Node:     false,
-		React:    true,
-		Next:     false,
-		Typesc:   true,
-		Duration: 10,
-	},
-}
+var node string
+var react string
+var next string
+var typesc string
 
 func main() {
+	connection.DatabaseConnect()
+
 	e := echo.New()
 	e.Static("/public", "public")
 	//Routing
@@ -62,7 +45,7 @@ func main() {
 	e.POST("/edit/:id", editFunc)
 
 	e.Logger.Fatal(e.Start("localhost:5000"))
-	
+
 }
 
 func home(c echo.Context) error {
@@ -70,10 +53,40 @@ func home(c echo.Context) error {
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
 	}
-	projets := map[string]interface{}{
-		"Project": dataProject,
+
+	// map data project
+	data, _ := connection.Conn.Query(context.Background(), "SELECT id_project, name_project, start_date, end_date, desc_project,technologies, duration FROM tb_project")
+
+	var result []Project
+	for data.Next() {
+		var each = Project{}
+		var techArray pgtype.VarcharArray
+		err := data.Scan(&each.Id, &each.Title, &each.Start, &each.End, &each.Desc, &techArray, &each.Duration)
+		if err != nil {
+			fmt.Println(err.Error())
+			return c.JSON(http.StatusInternalServerError, map[string]string{"Message": err.Error()})
+		}
+		each.Techno = make([]string, len(techArray.Elements))
+		for i, e := range techArray.Elements {
+			if e.String == "node" {
+				each.Techno[i] = "node"
+			} else if e.String == "react" {
+				each.Techno[i] = "react"
+			} else if e.String == "next" {
+				each.Techno[i] = "next"
+			} else if e.String == "typesc" {
+				each.Techno[i] = "typesc"
+			}
+		}
+
+		result = append(result, each)
 	}
-	
+	alert := fmt.Sprintf("<script>alert('Input data berhasil!')</script>")
+
+	projets := map[string]interface{}{
+		"Project": result,
+		"message":alert,
+	}
 	return tmpl.Execute(c.Response(), projets)
 }
 
@@ -90,55 +103,56 @@ func addProject(c echo.Context) error {
 	start := c.FormValue("start")
 	end := c.FormValue("end")
 	desc := c.FormValue("desc")
-	node := c.FormValue("node") == "on"
-	react := c.FormValue("react") == "on"
-	next := c.FormValue("next") == "on"
-	typesc := c.FormValue("typesc") == "on"
+	if c.FormValue("node") == "on" {
+		node = "node"
+	}
+	if c.FormValue("react") == "on" {
+		react = "react"
+	}
+	if c.FormValue("next") == "on" {
+		next = "next"
+	}
+	if c.FormValue("typesc") == "on" {
+		typesc = "typesc"
+	}
 	layout := "2006-01-02"
 	date1, _ := time.Parse(layout, start)
 	date2, _ := time.Parse(layout, end)
 	diff := date2.Sub(date1)
-
-	var addProject = Project{
-		Title:    title,
-		Desc:     desc,
-		Start:    start,
-		End:      end,
-		Node:     node,
-		React:    react,
-		Next:     next,
-		Typesc:   typesc,
-		Duration: int(diff.Hours()),
+	techs := []string{
+		node,
+		react,
+		next,
+		typesc,
 	}
+	_, err := connection.Conn.Exec(context.Background(), "insert into tb_project(name_project,start_date,end_date,desc_project,technologies,duration)values($1,$2,$3,$4,$5,$6)", title, start, end, desc, techs, diff.Hours()/24)
 
-	fmt.Println(start, end, diff)
-	dataProject = append(dataProject, addProject)
-
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+	success := "User data has been submitted successfully."
+	c.HTML(http.StatusOK,"<script>alert('" + success + "')")
 	return c.Redirect(http.StatusMovedPermanently, "/")
 }
 
 func editProject(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
+	// id, _ := strconv.Atoi(c.Param("id"))
 	tmpl, err := template.ParseFiles("views/edit-project.html")
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
 	}
 	var editProject = Project{}
-	for i, data := range dataProject {
-		if id == i {
-			editProject = Project{
-				Id: id,
-				Title:  data.Title,
-				Desc:   data.Desc,
-				Start:  data.Start,
-				End:    data.End,
-				Node:   data.Node,
-				React:  data.React,
-				Next:   data.Next,
-				Typesc: data.Typesc,
-			}
-		}
-	}
+	// for i, data := range dataProject {
+	// 	if id == i {
+	// 		editProject = Project{
+	// 			Id:    id,
+	// 			Title: data.Title,
+	// 			Desc:  data.Desc,
+	// 			Start: data.Start,
+	// 			End:   data.End,
+	// 		}
+	// 	}
+	// }
 	data := map[string]interface{}{
 		"Edit": editProject,
 	}
@@ -146,55 +160,50 @@ func editProject(c echo.Context) error {
 }
 
 func editFunc(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-	title := c.FormValue("title")
-	start := c.FormValue("start")
-	end := c.FormValue("end")
-	desc := c.FormValue("desc")
-	node := c.FormValue("node") == "on"
-	react := c.FormValue("react") == "on"
-	next := c.FormValue("next") == "on"
-	typesc := c.FormValue("typesc") == "on"
+	// id, _ := strconv.Atoi(c.Param("id"))
+	// title := c.FormValue("title")
+	// start := c.FormValue("start")
+	// end := c.FormValue("end")
+	// desc := c.FormValue("desc")
 
-	layout := "2006-01-02"
-	date1, _ := time.Parse(layout, start)
-	date2, _ := time.Parse(layout, end)
-	diff := date2.Sub(date1)
-	dataProject[id].Title = title
-	dataProject[id].Desc = desc
-	dataProject[id].Start = start
-	dataProject[id].End = end
-	dataProject[id].Node = node
-	dataProject[id].React = react
-	dataProject[id].Next = next
-	dataProject[id].Typesc = typesc
-	dataProject[id].Duration = int(diff.Hours())
+	// layout := "2006-01-02"
+	// date1, _ := time.Parse(layout, start)
+	// date2, _ := time.Parse(layout, end)
+	// diff := date2.Sub(date1)
+	// dataProject[id].Title = title
+	// dataProject[id].Desc = desc
+	// dataProject[id].Start = start
+	// dataProject[id].End = end
+
+	// dataProject[id].Duration = int(diff.Hours())
 	return c.Redirect(http.StatusMovedPermanently, "/")
 }
 
 func detailproject(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
 	tmpl, err := template.ParseFiles("views/detail-project.html")
+	
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
 	}
-	var detailProject = Project{}
 
-	for i, data := range dataProject {
-		if id == i {
-			detailProject = Project{
-				Title:    data.Title,
-				Desc:     data.Desc,
-				Start:    data.Start,
-				End:      data.End,
-				Node:     data.Node,
-				React:    data.React,
-				Next:     data.Next,
-				Typesc:   data.Typesc,
-				Duration: data.Duration,
-			}
-		}
+	var detailProject = Project{}
+	errs := connection.Conn.QueryRow(context.Background(), "SELECT id_project,name_project,start_date,end_date,desc_project,technologies,duration FROM tb_project WHERE id_project=$1", id).Scan(&detailProject.Id, &detailProject.Title, &detailProject.Start, &detailProject.End, &detailProject.Desc, &detailProject.Techno, &detailProject.Duration)
+	if errs != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
 	}
+
+	// for i, data := range dataProject {
+	// 	if id == i {
+	// 		detailProject = Project{
+	// 			Title:    data.Title,
+	// 			Desc:     data.Desc,
+	// 			Start:    data.Start,
+	// 			End:      data.End,
+	// 			Duration: data.Duration,
+	// 		}
+	// 	}
+	// }
 	data := map[string]interface{}{
 		"Project": detailProject,
 	}
@@ -205,7 +214,11 @@ func detailproject(c echo.Context) error {
 func deleteProject(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
 
-	dataProject = append(dataProject[:id], dataProject[id+1:]...)
+_, err := connection.Conn.Exec(context.Background(), "DELETE FROM tb_project WHERE id_project=$1", id)
+	
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError,map[string]string{"message":err.Error()})
+	}
 	return c.Redirect(http.StatusMovedPermanently, "/")
 }
 
